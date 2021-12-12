3c6f6776f2bb5caefa749ea17612ce33e4d90dcb
Akshay2002Singh <06akshay2002@gmail.com>
2021-12-13 00:57:28 +0530

    email sending added

--- home/views.py
+++ home/views.py
@@ -63,6 +63,20 @@ def submit_contact_form(request):
         temp_contact = contact(name=name,email=email,phone=phone,message=message)
         temp_contact.save()
         messages.success(request,"Your form has been submitted succesfully")
+
+        # send msg to admin
+        subject = f'Got complain from {name}'
+        message = f'Issue : {message}'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ['06akshay2002@gmail.com', ]
+        send_mail( subject, message, email_from, recipient_list )
+
+        # send msg to user
+        subject = f'Contact E-library'
+        message = f'Hello {name},\nOur team will contact you soon.\nThank you'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [email, ]
+        send_mail( subject, message, email_from, recipient_list )
         return render(request,'contact_us.html',context)
 
 def login_user(request):
